2779ed7427f05311e76fb19190f37dd9d0d55f20
tuttnauer-hq <tuttnauer-hq@users.noreply.github.com>
2019-11-17 22:52:30 +0000

    Automated deployment: Sun Nov 17 22:52:30 UTC 2019 73a3b12db606606fe5e95fbf1feb535e9b16146a

---
+++ TFSRepositories/precache-manifest.92bead8270734600b81409f8255316be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6806ddada2a8eb3340807650f4b86ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d59da9ffa1730773b25",
+    "url": "/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "cf718b48d7318230493b",
+    "url": "/static/js/2.f5d5ed4b.chunk.js"
+  },
+  {
+    "revision": "8d59da9ffa1730773b25",
+    "url": "/static/js/main.db2d63a3.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
