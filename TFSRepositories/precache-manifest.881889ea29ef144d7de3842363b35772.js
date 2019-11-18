86033095247c205f0360c7ea9f842a6b0d57a6bb
tamiradler <tamiradler@users.noreply.github.com>
2019-11-18 19:39:19 +0000

    Automated deployment: Mon Nov 18 19:39:19 UTC 2019 56ba9c9917ff5f3b2b3e442c14653245bb9ff995

---
+++ TFSRepositories/precache-manifest.881889ea29ef144d7de3842363b35772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00064cf1535840a5a8a8c63825b3543",
+    "url": "./relativepath/index.html"
+  },
+  {
+    "revision": "8d59da9ffa1730773b25",
+    "url": "./relativepath/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "cf718b48d7318230493b",
+    "url": "./relativepath/static/js/2.f5d5ed4b.chunk.js"
+  },
+  {
+    "revision": "8d59da9ffa1730773b25",
+    "url": "./relativepath/static/js/main.db2d63a3.chunk.js"
+  },
+  {
+    "revision": "dd706162df5117d1e268",
+    "url": "./relativepath/static/js/runtime-main.2366a549.js"
+  }
+]);
--- TFSRepositories/service-worker.js
+++ TFSRepositories/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "REACT_APP_API_URL/precache-manifest.38ae2d4376b35e73ae9b9ed13ae075a0.js"
+  "./relativepath/precache-manifest.881889ea29ef144d7de3842363b35772.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("REACT_APP_API_URL/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./relativepath/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
